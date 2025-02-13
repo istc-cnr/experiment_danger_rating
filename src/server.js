bc1d420506400793558086b3efc99cc25c0303e5
Natalia Kuzminykh <natkuzminykh94@gmail.com>
2025-02-13 17:41:08 +0100

    path adjustments

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.'));
+
+app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
